225fcb48fbb29c6e81322351fc7ec9b95eeb5f3a
ukiyun <byoa91@gmail.com>
2023-12-08 19:22:43 +0000

    FSI Week11 init

---
+++ FSI/Week10/CTF/cipherspec.py
@@ -0,0 +1,26 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
+
+KEYLEN = 16
+
+def gen(): 
+	offset = 3 # Hotfix to make Crypto blazing fast!!
+	key = bytearray(b'\x00'*(KEYLEN-offset)) 
+	key.extend(os.urandom(offset))
+	return bytes(key)
+
+def enc(k, m, nonce):
+	cipher = Cipher(algorithms.AES(k), modes.CTR(nonce))
+	encryptor = cipher.encryptor()
+	cph = b""
+	cph += encryptor.update(m)
+	cph += encryptor.finalize()
+	return cph
+
+def dec(k, c, nonce):
+	cipher = Cipher(algorithms.AES(k), modes.CTR(nonce))
+	decryptor = cipher.decryptor()
+	msg = b""
+	msg += decryptor.update(c)
+	msg += decryptor.finalize()
+	return msg
